62782fe26b11c59d5c3209d4d5e52fc118e1bcd6
Nirtendra yadav <nirtendraydv@gmail.com>
2025-08-03 06:59:04 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -25,7 +25,7 @@ const InstagramIcon = (props: React.SVGProps<SVGSVGElement>) => (
     <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" strokeWidth="0" strokeLinecap="round" strokeLinejoin="round">
         <rect width="20" height="20" x="2" y="2" rx="5" ry="5" fill="none" stroke="currentColor" strokeWidth="2"/>
         <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
-        <line x1="17.5" x2="17.51" y1="6.5" y2="6.5" stroke-width="2"/>
+        <line x1="17.5" x2="17.51" y1="6.5" y2="6.5" strokeWidth="2"/>
     </svg>
 );
 
@@ -184,3 +184,5 @@ export default function Header() {
     </header>
   );
 }
+
+    
