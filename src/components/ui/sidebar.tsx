eb9dc387c03a147ac90ceef9a278c01da2a39956
Nirtendra yadav <nirtendraydv@gmail.com>
2025-08-03 09:41:16 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -10,7 +11,7 @@ import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
-import { Sheet, SheetContent } from "@/components/ui/sheet"
+import { Sheet, SheetContent, SheetHeader, SheetTitle } from "@/components/ui/sheet"
 import { Skeleton } from "@/components/ui/skeleton"
 import {
   Tooltip,
@@ -176,6 +177,11 @@ const Sidebar = React.forwardRef<
     ref
   ) => {
     const { isMobile, state, openMobile, setOpenMobile } = useSidebar()
+    
+    const sidebarHeader = React.Children.toArray(children).find(
+      (child) =>
+        React.isValidElement(child) && child.type === SidebarHeader
+    )
 
     if (collapsible === "none") {
       return (
@@ -206,6 +212,10 @@ const Sidebar = React.forwardRef<
             }
             side={side}
           >
+            <SheetHeader className="p-0 [&>div]:border-b [&>div]:p-2">
+                <SheetTitle className="sr-only">Sidebar Menu</SheetTitle>
+                {sidebarHeader}
+            </SheetHeader>
             <div className="flex h-full w-full flex-col">{children}</div>
           </SheetContent>
         </Sheet>
